f8874de6dc73d734e9f1594b932398bd4224a151
skruse <skruse@tvt.local>
2022-03-18 11:00:26 -0700

    added git ignore

---
+++ src/com/tvt/Main.java
@@ -0,0 +1,40 @@
+package com.tvt;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Whats good");
+
+        // convention
+        // camelCase standard
+
+        // Primitive Types
+        /*
+             Type
+            - byte      1   [-128, 127]
+            - short     2   [-32k , 32k]
+            - int       4   [-2B, 2B]
+            - long      8
+            - float     4
+            - double    8
+            - char      2   A, B, C, etc
+            - boolean   1   true / false
+         */
+
+
+
+
+        // Reference Types
+
+
+
+    }
+}
+
+/*
+
+
+
+ */
